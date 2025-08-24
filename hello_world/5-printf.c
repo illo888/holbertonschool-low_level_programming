af25b423ea036633003e87f95f12b3a67b51fcde
root <root@ip-10-42-46-63.ec2.internal>
2025-08-24 16:03:56 +0000

    add

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -4,10 +4,10 @@
  *
  * My function
  *
- * Return: Always 0 (Success)
+ * Return: always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
