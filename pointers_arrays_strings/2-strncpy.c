99b9d8dbe31dcd24ce2a67106829ee2c4ddfa7e3
root <root@ip-10-42-40-20.ec2.internal>
2025-09-03 15:52:29 +0000

    Write a function that copies a string.
    Prototype: char *_strncpy(char *dest, char *src, int n);
    Your function should work exactly like strncpy
    FYI: The standard library provides a similar function: strncpy. Run man strncpy to learn more.

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,27 +1,41 @@
-<<<<<<< HEAD
-=======
+#include "main.h"
+#include <stdio.h>
 
->>>>>>> 50881b9a14dbb999530c0a082e5fa1fbc7d0a947
 /**
- * _strncpy - copies a string
- * @dest: buffer where we copy the string
- * @src: the source string
- * @n: maximum number of bytes to copy
+ * main - check the code
  *
- * Return: pointer to dest
+ * Return: Always 0.
  */
-char *_strncpy(char *dest, char *src, int n)
+int main(void)
 {
-int i = 0;
-while (i < n && src[i] != '\0')
-{
-dest[i] = src[i];
-i++;
-}
-while (i < n)
-{
-dest[i] = '\0';
-i++;
-}
-return (dest);
+    char s1[98];
+    char *ptr;
+    int i;
+
+    for (i = 0; i < 98 - 1; i++)
+    {
+        s1[i] = '*';
+    }
+    s1[i] = '\0';
+    printf("%s\n", s1);
+    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
+    printf("%s\n", s1);
+    printf("%s\n", ptr);
+    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
+    printf("%s", s1);
+    printf("%s", ptr);
+    for (i = 0; i < 98; i++)
+    {
+        if (i % 10)
+        {
+            printf(" ");
+        }
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", s1[i]);
+    }
+    printf("\n");
+    return (0);
 }
