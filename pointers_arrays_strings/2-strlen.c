c7c20ead264010b72545d9f34a814b54413120d9
root <root@ip-10-42-70-94.ec2.internal>
2025-09-03 13:42:00 +0000

    Write a function that returns the length of a string.

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -14,30 +14,3 @@ int _strlen(char *s)
 	}
 	return (length);
 }
-
-
-
-
-
-New
-4:33
-التاسع
-#include "main.h"
-/**
- * _strcpy - copies the string pointed to by src to dest
- * @dest: destination buffer
- * @src: source string
- *
- * Return: pointer to dest
- */
-char *_strcpy(char *dest, char *src)
-{
-	int i = 0;
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
