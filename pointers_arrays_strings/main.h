e7ff41d943eeab9dbd27010faf8840d4fafa6941
root <root@ip-10-42-227-74.ec2.internal>
2025-09-04 14:56:44 +0000

    add 6-7 file

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -19,5 +19,7 @@ char *_strcpy(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *str);
+char *cap_string(char *);
+char *leet(char *s);
 
 #endif /* MAIN_H */
