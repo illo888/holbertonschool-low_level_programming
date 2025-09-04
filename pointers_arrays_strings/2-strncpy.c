80ddba1c13c7cbb4b14266d7fa0133ec1806e5d0
root <root@ip-10-42-227-74.ec2.internal>
2025-09-04 14:33:52 +0000

    ubdate

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,17 +1,27 @@
-#include "main.h"
 #include <stdio.h>
-char *strncpy(char *dest, char *src, int n)
+#include <string.h>
+
+/**
+ * _strncpy - copies a string
+ *
+ * @dest: pointer to an char
+ * @src: pointer to char
+ * @n: int
+ *
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
 {
-    int i;
+	int i = strlen(src);
+	int j;
 
-    for (i = 0; i < n && src[i] != '\0'; i++)
-    {
-        dest[i] = src[i];
-    }
-    for (; i < n; i++)
-    {
-        dest[i] = '\0';
-    }
-
-    return dest;
+	for (j = 0 ; (j < n) && (j < i) ; j++)
+		dest[j] = src[j];
+	if (j == i)
+	{
+		for ( ; i < n ; i++)
+			dest[i] = '\0';
+	}
+	return (dest);
 }
