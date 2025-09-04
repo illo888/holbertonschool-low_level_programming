823c15c27af59b83aa777fc689645afd204f7613
root <root@ip-10-42-227-74.ec2.internal>
2025-09-04 14:10:06 +0000

    update

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -15,5 +15,5 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
-
+char *_strcpy(char *dest, char *src);
 #endif /* MAIN_H */
