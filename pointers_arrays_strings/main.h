2f4f7ee9272c83493d33cfd452eb0d37c3509fa4
root <root@ip-10-42-5-66.ec2.internal>
2025-09-09 14:41:54 +0000

    The _memset() function fills the first n bytes of the memory area pointed to by s with the constant byte b Returns a pointer to the memory area s

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -21,5 +21,5 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *);
 char *leet(char *s);
-
+char *_memset(char *s, char b, unsigned int n);
 #endif /* MAIN_H */
