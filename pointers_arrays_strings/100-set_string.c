d37e87d3739b7810ac601305b4de0fc5ec14bf07
root <root@ip-10-42-169-10.ec2.internal>
2025-09-12 19:28:16 +0000

    Write a function that sets the value of a pointer to a char.

--- pointers_arrays_strings/100-set_string.c
+++ pointers_arrays_strings/100-set_string.c
@@ -1,30 +1,11 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * set_string - sets the value of a pointer to a char
- * @s: pointer to a pointer to a char
- * @to: pointer to the char we want to point to
+ * @s: pointer to pointer
+ * @to: string to point to
  */
 void set_string(char **s, char *to)
 {
-    *s = to;
+	*s = to;
 }
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char *s = "Bob Dylan";
-    char *f = "Robert Allen";
-
-    printf("Before: %s, %s\n", s, f);
-    set_string(&s, f);
-    printf("After: %s, %s\n", s, f);
-
-    return (0);
-}
-
