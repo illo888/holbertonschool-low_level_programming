46982ddd25ecfb84705e10d908fffd54badef6f2
root <root@ip-10-42-169-10.ec2.internal>
2025-09-12 22:17:13 +0000

    Write a function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -1,28 +1,29 @@
 #include "main.h"
 
 /**
- * wildcmp - compares two strings, allowing wildcard '*' in s2
- * @s1: the first string
- * @s2: the second string (may contain '*')
- *
- * Return: 1 if identical, 0 otherwise
- */
+* wildcmp - compares two strings, allowing wildcard '*' in s2
+* @s1: the first string
+* @s2: the second string (may contain '*')
+*
+* Return: 1 if identical, 0 otherwise
+*/
 int wildcmp(char *s1, char *s2)
 {
-    if (*s2 == '\0')
-        return (*s1 == '\0');
+if (*s2 == '\0')
+return (*s1 == '\0');
 
-    if (*s2 == '*')
-    {
-        if (*(s2 + 1) == '\0')
-            return (1);
-        if (wildcmp(s1, s2 + 1))
-            return (1);
-        return (*s1 != '\0' && wildcmp(s1 + 1, s2));
-    }
-
-    if (*s1 == *s2)
-        return (wildcmp(s1 + 1, s2 + 1));
-
-    return (0);
+if (*s2 == '*')
+{
+if (*(s2 + 1) == '\0')
+return (1);
+if (wildcmp(s1, s2 + 1))
+return (1);
+return (*s1 != '\0' && wildcmp(s1 + 1, s2));
 }
+
+if (*s1 == *s2)
+return (wildcmp(s1 + 1, s2 + 1));
+
+return (0);
+}
+
