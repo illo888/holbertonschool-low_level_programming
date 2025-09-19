985b6c644afd821c3f10bc06937822dd7cbe818c
root <root@ip-10-42-250-120.ec2.internal>
2025-09-19 11:12:11 +0000

    Write a function that concatenates all the arguments of your program.

--- malloc_free/100-argstostr.c
+++ malloc_free/100-argstostr.c
@@ -4,30 +4,30 @@
 
 char *argstostr(int ac, char **av)
 {
-    int i, j, k = 0, len = 0;
-    char *str;
+int i, j, k = 0, len = 0;
+char *str;
 
-    if (ac == 0 || av == NULL)
-        return (NULL);
+if (ac == 0 || av == NULL)
+return (NULL);
 
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j]; j++)
-            len++;
-        len++;
-    }
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j]; j++)
+len++;
+len++;
+}
 
-    str = malloc(sizeof(char) * (len + 1));
-    if (str == NULL)
-        return (NULL);
+str = malloc(sizeof(char) * (len + 1));
+if (str == NULL)
+return (NULL);
 
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j]; j++)
-            str[k++] = av[i][j];
-        str[k++] = '\n';
-    }
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j]; j++)
+str[k++] = av[i][j];
+str[k++] = '\n';
+}
 
-    str[k] = '\0';
-    return (str);
+str[k] = '\0';
+return (str);
 }
