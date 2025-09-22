b8d83e4cace67fa219312792569b4107f798f79b
root <root@ip-10-42-197-189.ec2.internal>
2025-09-22 12:38:52 +0000

    Write a function that prints a name.

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-#include <stddef.h>        /* for NULL */
+#include <stddef.h>
 #include "function_pointers.h"
 
 /**
@@ -12,6 +12,5 @@ void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
-
 	f(name);
 }
