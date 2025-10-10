18f467f94aab6af5278650d3aca56a0cc09a255e
root <root@ip-10-42-131-139.ec2.internal>
2025-10-10 07:05:25 +0000

    edit

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -3,15 +3,15 @@
 #include "lists.h"
 
 /**
-* add_node_end - adds a new node at the end of a list_t list
-* @head: double pointer to the head of the list
-* @str: string to duplicate and store in the new node
+* add_node - adds a new node at the beginning of a list_t list
+* @head: pointer to pointer to the head of the list
+* @str: string to duplicate and add to the new node
 *
 * Return: address of the new element, or NULL if it failed
 */
-list_t *add_node_end(list_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node, *temp;
+	list_t *new_node;
 	unsigned int len = 0;
 
 	if (str == NULL)
@@ -21,6 +21,9 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (new_node == NULL)
 		return (NULL);
 
+	while (str[len])
+		len++;
+
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
@@ -28,23 +31,9 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	while (str[len])
-		len++;
-
 	new_node->len = len;
-	new_node->next = NULL;
-
-	if (*head == NULL)
-	{
-		*head = new_node;
-	}
-	else
-	{
-		temp = *head;
-		while (temp->next != NULL)
-			temp = temp->next;
-		temp->next = new_node;
-	}
+	new_node->next = *head;
+	*head = new_node;
 
 	return (new_node);
 }
